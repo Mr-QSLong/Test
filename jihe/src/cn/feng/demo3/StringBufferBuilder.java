998369720c9508762ea68004f43af9d816d07a03
Mr-QSLong <mr_qin0525@163.com>
2019-04-22 10:58:14 +0800

    a

--- jihe/src/cn/feng/demo3/StringBufferBuilder.java
+++ jihe/src/cn/feng/demo3/StringBufferBuilder.java
@@ -6,6 +6,7 @@ public class StringBufferBuilder {
         String a = "hello2";
         String b = "hello" + 2;
         System.out.println((a == b));
+        System.out.println("lalalal");
     }
 
     public void testAgain(){
